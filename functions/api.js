73fd85122d0bb8a935fb6ab9c625f6d381386446
fisnikrexhepi <fisnik@frakton.com>
2023-04-03 21:00:29 +0200

    first commit

--- functions/api.js
+++ functions/api.js
@@ -1,50 +1,65 @@
-const express = require('express');
-const serverless = require('serverless-http');
+const express = require("express");
 const app = express();
 const router = express.Router();
+const serverless = require("serverless-http");
+const chromium = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer-core");
+const CsvParser = require("json2csv").Parser;
 
-let records = [];
-
-//Get all students
-router.get('/', (req, res) => {
-  res.send('App is running..');
+router.get("/", (req, res) => {
+  res.send("App is running..");
 });
 
-//Create new record
-router.post('/add', (req, res) => {
-  res.send('New record added.');
+router.post("", async (req, res) => {
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    executablePath:
+      "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" ||
+      (await chromium.executablePath),
+    headless: true,
+  });
+
+  if (req.body) {
+    const keyword = req.body.keyword;
+    console.log(`Scraping URLs for keyword: ${keyword}`);
+    const page = await browser.newPage();
+    const url = `https://www.google.com/search?q=${keyword}`;
+    await page.goto(url);
+    let urls = [];
+    while (urls.length < 30) {
+      const currentUrls = await page.evaluate(() => {
+        const links = Array.from(document.querySelectorAll("a"));
+        const urls = links.map((link) => link.href);
+        const nonGoogleUrls = urls.filter((url) => {
+          return (
+            !url.includes("google.") &&
+            !url.includes("webcache.googleusercontent.com")
+          );
+        });
+        return nonGoogleUrls;
+      });
+      urls = [...urls, ...currentUrls];
+      urls = Array.from(new Set(urls));
+      const nextButton = await page.$("#pnnext");
+      if (urls.length >= 30 || !nextButton) {
+        break;
+      }
+      await Promise.all([
+        page.waitForNavigation({ waitUntil: "networkidle2" }),
+        page.click("#pnnext"),
+      ]);
+    }
+    const foundedUrls = urls.slice(0, 30).map((url) => ({ url }));
+    console.log(`Finished scraping URLs`);
+    await browser.close();
+    foundedUrls.splice(0, 1);
+    const csvParser = new CsvParser({});
+    const csvData = csvParser.parse(foundedUrls);
+    res.setHeader("Content-Type", "text/csv");
+    res.setHeader("Content-Disposition", "attachment; filename=tutorials.csv");
+    res.status(200).end(csvData);
+  }
 });
 
-//delete existing record
-router.delete('/', (req, res) => {
-  res.send('Deleted existing record');
-});
-
-//updating existing record
-router.put('/', (req, res) => {
-  res.send('Updating existing record');
-});
-
-//showing demo records
-router.get('/demo', (req, res) => {
-  res.json([
-    {
-      id: '001',
-      name: 'Smith',
-      email: 'smith@gmail.com',
-    },
-    {
-      id: '002',
-      name: 'Sam',
-      email: 'sam@gmail.com',
-    },
-    {
-      id: '003',
-      name: 'lily',
-      email: 'lily@gmail.com',
-    },
-  ]);
-});
-
-app.use('/.netlify/functions/api', router);
+app.use("/.netlify/functions/api", router);
 module.exports.handler = serverless(app);
